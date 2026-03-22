dbdaca3f142904cbf77aed4cc0b3c146bedc4749
Eliezer <jeliezercapuanolopez@gmail.com>
2026-03-22 12:51:22 -0400

    feat: :construction: [Work updation] Small changes for Ios building

--- lib/src/utils/theme.dart
+++ lib/src/utils/theme.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/services.dart';
 
 class ThemeUtils {
-  static setStatusBarAndNavigationBarTheme(
-      {required Color color,
-      required Brightness brightness,
-      Brightness? brightnessNavigationBar,
-      Color? navigationBarColor,
-      bool changeNavigationBrightness = true}) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarIconBrightness: brightness,
-      systemNavigationBarIconBrightness: changeNavigationBrightness
-          ? (brightnessNavigationBar ?? brightness)
-          : null,
-      statusBarColor: color, //any color of your choice
-      systemNavigationBarColor: navigationBarColor ?? color,
-    ));
+  static void setStatusBarAndNavigationBarTheme({
+    required Color color,
+    required Brightness brightness,
+    Brightness? brightnessNavigationBar,
+    Color? navigationBarColor,
+    bool changeNavigationBrightness = true,
+  }) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: brightness,
+        systemNavigationBarIconBrightness: changeNavigationBrightness
+            ? (brightnessNavigationBar ?? brightness)
+            : null,
+        statusBarColor: color, //any color of your choice
+        systemNavigationBarColor: navigationBarColor ?? color,
+      ),
+    );
   }
 }
